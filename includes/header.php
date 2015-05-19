48f64e38cb0862426edb46de8d0767b991673282
George Offley <goffley3@aol.com>
2015-05-19 18:16:16 -0400

    changed directory and converted to PHP

---
+++ includes/header.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html>
+<head>
+	<title>The New TYT Network</title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="styles/styles.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+	<!--Section with the registration call to action-->
+	<section class="container-fluid">
+		<section class="action-button-head row">
+			<div class="col-md-12 wrap">
+				<div class="pull-left">
+					<div class="welcome">
+						<h4>Welcome to the TYT Network</h4>
+					</div>
+					<h3>Join Us!</h3>
+				</div>
+				<div class="pull-right action-head-buttons">
+					<button type="button" class="btn btn-primary"><b>Register</b></button>
+					<button type="button" class="btn btn-default">Login</button>
+				</div>
+			</div>
+		</section>
+	</section>
+	<section class="container-fluid wrap">
+		<header class="row">
+			<article class="col-md-12">
+				<img src="http://tyt.wpengine.netdna-cdn.com/wp-content/uploads/cropped-TYTNetwork-logo-2014-520x180.png" class="logo img-responsive">
+			</article>
+		</header>
