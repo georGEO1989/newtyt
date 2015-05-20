f51b1160d77566c1a63cbf6f7db8f1a78953f1f8
George Offley <goffley3@aol.com>
2015-05-19 21:45:07 -0400

    last commit 05/19

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -1,36 +1,5 @@
 var ytApp = angular.module('YouTubeApp', []);
-/*
-ytApp.directive('youtube', function($window) {
-	return {
-		restrict: "E",
 
-		scope: {
-			height: "@",
-			width: "@",
-			videoId: "@"
-		},
-
-		template: '<div></div>',
-
-		link: function(scope, element) {
-			var tag = document.createElement('script');
-			tag.src = "https://www.youtube.com/iframe_api";
-			var firstScriptTag = document.getElementsByTagName('scope')[0];
-			firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-			var player;
-
-			$window.onYouTubeIframeAPIReady = function() {
-				player = new YT.Player(element.children()[0], {
-					height: scope.height,
-					width: scope.width,
-					videoId: scope.videoId
-				});
-			};
-		},
-	}
-});
-*/
 ytApp.directive('youtube', function($window) {
   return {
     restrict: "E",
