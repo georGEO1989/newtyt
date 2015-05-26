f92ace98eb251d4246a9a397ffe4d28fe26e822f
George Offley <goffley3@aol.com>
2015-05-26 15:19:12 -0400

    test

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -1,10 +1,24 @@
 'use strict';
 
 angular.module('newtytApp')
-	.controller('LoginCtrl', function ($scope) {
+	.controller('LoginCtrl', function ($scope, $rootScope, AUTH_EVENTS, AuthService) {
+		/*
 		$scope.awesomeThings = [
 	        'HTML5 Boilerplate',
       		'AngularJS',
       		'Karma'
 		];
+		*/
+		$scope.credentials = {
+			username: '',
+			password: ''
+		};
+		$scope.login = function (credentials) {
+			AuthService.login(credentials).then(function (user) {
+				$rootScope.$broadcast(AUTH_EVENTS.loginSuccess);
+				$scope.setCurrentUser(user);
+			}, function () {
+				$rootScope.$broadcast(AUTH_EVENTS.loginFailed);
+			});
+		};
 	});
